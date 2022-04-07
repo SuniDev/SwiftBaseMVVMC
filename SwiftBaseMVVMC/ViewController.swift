8f44e360ccce60d2afb39b7380985d878d6cfc43
suni <sun-918@naver.com>
2022-04-07 16:36:23 +0900

    create : project

---
+++ SwiftBaseMVVMC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftBaseMVVMC
+//
+//  Created by suni on 2022/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
